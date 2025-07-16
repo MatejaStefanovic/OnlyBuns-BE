f799115fa7b07215d8904f5af3ceccb12c6daec3
MatejaStefanovic <matejastefanovic214@gmail.com>
2025-07-16 21:44:13 +0200

    Message broker done

--- messagebroker/src/main/java/org/messagebroker/model/MessageRequest.java
+++ messagebroker/src/main/java/org/messagebroker/model/MessageRequest.java
@@ -1,17 +1,20 @@
 package org.messagebroker.model;
 
 public class MessageRequest {
-    private String appId;
+    private String senderId;
+    private String targetId;
     private String queueId;
     private Message message;
-
-    public String getAppId() { return appId; }
-    public void setAppId(String appId) { this.appId = appId; }
-
+    
+    public String getSenderId() { return senderId; }
+    public void setSenderId(String senderId) { this.senderId = senderId; }
+    
+    public String getTargetId() { return targetId; }
+    public void setTargetId(String targetId) { this.targetId = targetId; }
+    
     public String getQueueId() { return queueId; }
     public void setQueueId(String queueId) { this.queueId = queueId; }
-
+    
     public Message getMessage() { return message; }
     public void setMessage(Message message) { this.message = message; }
 }
-
